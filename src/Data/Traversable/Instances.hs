b8bc7304069768c63b0859fcabdf14ea7863f0e8
exergasia <exergasia.expolitio.repetition@gmail.com>
2014-07-31 00:48:36 +0100

    Provide Foldable/Traversable for tuples on pre-7.8 GHC

--- src/Data/Traversable/Instances.hs
+++ src/Data/Traversable/Instances.hs
@@ -3,14 +3,27 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Data.Traversable.Instances where
 
-#if !(MIN_VERSION_transformers(0,3,0))
-import Control.Monad.Trans.Identity
+#if (!(MIN_VERSION_base(4,7,0)) || !(MIN_VERSION_transformers(0,3,0)))
 import Data.Foldable
 import Data.Traversable
+#if !(MIN_VERSION_transformers(0,3,0))
+import Control.Monad.Trans.Identity
+#endif
 
+#if !(MIN_VERSION_transformers(0,3,0))
 instance Foldable m => Foldable (IdentityT m) where
   foldMap f = foldMap f . runIdentityT
 
 instance Traversable m => Traversable (IdentityT m) where
   traverse f = fmap IdentityT . traverse f . runIdentityT
+#endif
+
+#if !(MIN_VERSION_base(4,7,0))
+instance Foldable ((,) a) where
+  foldMap f (_, x) = f x
+
+instance Traversable ((,) a) where
+  traverse f (a, b) = (,) a <$> f b
+#endif
+
 #endif
